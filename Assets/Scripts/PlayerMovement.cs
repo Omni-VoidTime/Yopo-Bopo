0a4d8fa99564f7c690fbc0f1f063a21b39d62ada
epps010 <epps010@csusm.edu>
2025-11-06 00:10:16 -0800

    Added basic horizontal movement and jump functionality. Jump charges with space then on release jumps.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,57 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerMovement : MonoBehaviour
 {
-       public float moveSpeed = 5f;
+    public float moveSpeed = 5f;
+
+    [Header("Jump")]
+    public float minJumpForce = 5f;
+    public float maxJumpForce = 15f;
+    public float chargeRate = 20f;   // how fast power fills per second
 
     private Rigidbody2D rb;
-    private Vector2 moveInput; // cached direction from the event
+    private float horizontal;
+    private bool isChargingJump;
+    private float jumpPower;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // assign to the Move event
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        moveInput = ctx.ReadValue<Vector2>();
+        var v = ctx.ReadValue<Vector2>();
+        horizontal = v.x;           // only x matters now
+    }
+
+    // called from input action (performed on press, canceled on release)
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)  // space down
+        {
+            isChargingJump = true;
+            jumpPower = minJumpForce;
+        }
+        else if (ctx.canceled) // space released
+        {
+            isChargingJump = false;
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    private void Update()
+    {
+        if (isChargingJump)
+        {
+            jumpPower += chargeRate * Time.deltaTime;
+            jumpPower = Mathf.Clamp(jumpPower, minJumpForce, maxJumpForce);
+        }
     }
 
     private void FixedUpdate()
     {
-        // new unity RB API prefers linearVelocity in 6.x
-        rb.linearVelocity = moveInput * moveSpeed;
-
-        // if your build complains:
-        // rb.velocity = moveInput * moveSpeed;
+        rb.linearVelocity = new Vector2(horizontal * moveSpeed, rb.linearVelocity.y);
     }
 }
