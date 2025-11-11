5aae788911b61cbfdc93755d620148c0fc96bf8d
themixedmaster <morri213@csusm.edu>
2025-11-10 16:15:45 -0800

    player movement stuff and start of level design

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -15,6 +16,11 @@ public class PlayerMovement : MonoBehaviour
     private float horizontal;
     private bool isJumping;
 
+    public bool touchingFloor = false;
+    public bool touchingRightWall = false;
+    public bool touchingLeftWall = false;
+    public bool touchingCeiling = false;
+
     int jumpFrame = 0;
 
     private void Awake()
@@ -26,12 +32,14 @@ public class PlayerMovement : MonoBehaviour
     {
         var v = ctx.ReadValue<Vector2>();
         horizontal = v.x;           // only x matters now
+        
     }
 
     // called from input action (performed on press, canceled on release)
     public void OnJump(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)  // space down
+        Debug.Log("jump");
+        if (ctx.performed && touchingFloor)  // space down and can jump
         {
             isJumping = true;
         }
@@ -50,7 +58,66 @@ public class PlayerMovement : MonoBehaviour
             //float jumpPower = Mathf.Clamp(jumpForce, minJumpForce, maxJumpForce);
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-        if(horizontal != 0)
+        //set movement to zero if colliding with that wall
+        //(this prevents players sticking to walls when they shouldn't be able to)
+        if ((horizontal > 0 && touchingRightWall) || (horizontal < 0 && touchingLeftWall))
+        {
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+        }
+        else
+        {
             rb.linearVelocity = new Vector2(moveSpeed * horizontal, rb.linearVelocity.y);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        HandleConstantCollision(collision);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        HandleConstantCollision(collision);
+    }
+
+    //called from collision enter and collision stay
+    void HandleConstantCollision(Collision2D collision)
+    {
+        List<ContactPoint2D> points = new List<ContactPoint2D>();
+        collision.GetContacts(points);
+        foreach (ContactPoint2D contact in points)
+        {
+            Vector2 relativeLocation = contact.point - (Vector2)transform.position;
+            //Debug.Log(relativeLocation.ToString());
+            float x = relativeLocation.x;
+            float y = relativeLocation.y;
+            //see where surface is relative to player
+            if (y < x && y < -x)
+            {
+                touchingFloor = true;
+            }
+            else if (y > x && y < -x)
+            {
+                touchingLeftWall = true;
+            }
+            else if (y < x && y > -x)
+            {
+                touchingRightWall = true;
+            }
+            else
+            {
+                touchingCeiling = true;
+            }
+
+        }
+    }
+
+    //uncheck all environment collisions when no longer colliding with something
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        touchingFloor = false;
+        touchingLeftWall = false;
+        touchingRightWall = false;
+        touchingCeiling = false;
     }
 }
